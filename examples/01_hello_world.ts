7a74aa1b744c7e4aaef2633b798698645210f10e
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2022-04-11 20:28:12 +0200

    Added 01_hello_world.ts

---
+++ examples/01_hello_world.ts
@@ -0,0 +1,23 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const PORT = 8080;
+const server = serve({ port: PORT });
+console.log(`Your server is running on http://localhost:${PORT}/`);
+
+for await (const req of server) {
+
+//    req.respond({ body: "Hello World\n" });
+
+const response: Response = {}; // Create a Response instance, init with {}
+
+response.headers = new Headers(); // Create Headers object and assign to response
+response.headers.set("content-type", "application/json"); // set header as json
+
+const body = { message: "hello world" }; // Create response body
+
+// req.respond(body) // Error, cant assign object to body
+
+response.body = JSON.stringify(body); // Serialize to string bytes.
+
+req.respond(response); // respond response
+}
